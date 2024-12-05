6be8d24aed0114c1d92e3e0391d12e01a2e66fb4
EC2 Default User <ec2-user@ip-172-31-27-156.eu-west-1.compute.internal>
2024-12-05 19:14:18 +0000

    item added test

--- EcoGive/settings.py
+++ EcoGive/settings.py
@@ -163,7 +163,7 @@ EMAIL_PORT = 587  # Mailjet SMTP port
 EMAIL_USE_TLS = True
 
 #For AWS S3 Integration, please ensure S3 bucket name created in S3
-AWS_STORAGE_BUCKET_NAME = "x23340355-ecogive"
+AWS_STORAGE_BUCKET_NAME = os.getenv("AWS_STORAGE_BUCKET_NAME")
 AWS_S3_CUSTOM_DOMAIN = f'{AWS_STORAGE_BUCKET_NAME}.s3.amazonaws.com'
 AWS_DEFAULT_ACL = None
 AWS_LOCATION = 'media'
