abf5f9febda190395a892e82bf25c104f9ac0335
EC2 Default User <ec2-user@ip-172-31-27-156.eu-west-1.compute.internal>
2024-12-08 03:43:01 +0000

    modified buildspec.yml

--- EcoGive/settings.py
+++ EcoGive/settings.py
@@ -22,7 +22,6 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/4.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-#SECRET_KEY = 'django-insecure-7%k)n24ayo%q-+wk4*w+a*lgf+jja8hm-qp=y+-%fz##d-51_e'
 SECRET_KEY = os.getenv('DJANGO_SECRET_KEY', 'fallback-secret-for-local-use-only')
 
 # SECURITY WARNING: don't run with debug turned on in production!
